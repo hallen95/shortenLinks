10d41e01e0b65d2224d14170f96d139e009d0dd8
Brian <br.y.an@hotmail.com>
2021-04-22 00:15:05 -0300

    added mongodb for heroku

--- server.js
+++ server.js
@@ -1,50 +1,36 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const ShortUrl = require('./models/shortUrls');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const app = express()
 
-mongoose.connect("mongodb://localhost/urlShortener", {
-    useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect('mongodb+srv://fender2225:fender2225@cluster0.zq2ft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true
 })
 
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
+app.set('port', process.env.PORT || 5000)
 
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/",   (async (req, res) => {
-    try {
-        const shortUrls = await ShortUrl.find();
-        console.log(shortUrls)
-    res.render('index', { shortUrls: shortUrls});
-    }
-    catch(e){ 
-        console.log(new Error(e));
-    }
-}));
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
 
-app.post("./shortUrls",  (async(req, res) => {
-    try {
-        await ShortUrl.create({ full: req.body.fullUrl })
-        res.redirect("/")
-    }
-    catch(e){
-        console.log("error:", e)
-    }
-}));
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
 
-app.get('/:shortUrl', (async (req, res) => {
-    try {
-    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-    if (shortUrl == null) return res.sendStatus(404)
-  
-    shortUrl.clicks++
-    shortUrl.save()
-  
-    res.redirect(shortUrl.full)
-    }
-    catch(e){
-        console.log("error:", e)
-    }
-  }));
+  res.redirect('/')
+})
 
-app.listen(process.env.PORT || 5000);
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(app.get('port'));
