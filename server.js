1ef927b7877c8bd6d455cef6987ad95febb28c65
Brian <br.y.an@hotmail.com>
2021-04-22 17:37:22 -0300

    documentacion añadida

--- server.js
+++ server.js
@@ -3,26 +3,33 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrls')
 const app = express()
 
+/* esto funciona para una base de datos local, 
+pero para hacer el deploy en Heroku necesitamos otros datos */ 
 mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
+//con esto nos aseguramos que ejs sea el engine que usamos para tomar los datos del form
 app.set('view engine', 'ejs')
 app.set('port', process.env.PORT || 5000)
 
 app.use(express.urlencoded({ extended: false }))
 
+// con el metodo find que nos provee el schema podemos pintar en pantalla todos los links en /
 app.get('/', async (req, res) => {
   const shortUrls = await ShortUrl.find()
   res.render('index', { shortUrls: shortUrls })
 })
 
+// con el metodo create le pasamos los datos del req por prop 
 app.post('/shortUrls', async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl })
 
   res.redirect('/')
 })
 
+// este parametro dinamico tomara el link acortado cuando le hagamos click y 
+// lo convertirá en el original en caso de existir y sino enviará un status de error
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
   if (shortUrl == null) return res.sendStatus(404)
